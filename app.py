0677226efe855f0fda6883e9f5972fa91ae02617
gonzaloplaza <gonzaloplazagomez@gmail.com>
2021-01-18 09:00:36 -0300

    ejemplo base

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print(2 + 2)
+from flask import Flask
+
+
+# app instance
+app = Flask(__name__)
+
+# routes
+@app.route('/')
+def index():
+    return '<h1>Hola Mundo</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+
+# start
+if __name__ == '__main__':
+    app.run(debug=True)
